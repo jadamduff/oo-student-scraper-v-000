04ad1bfc44c24457ba1011cde844fa5eed1da284
Adam Duff <j.adam.duff@gmail.com>
2018-08-20 18:47:25 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,22 +7,12 @@ class Scraper
   def self.scrape_index_page(index_url)
     students_hash = {}
     doc = open(index_url)
-    students = Nokogiri::HTML(doc).css('.student-card')
-    students.each do |student|
-      puts "#{student.css('a h4.student-name').text}"
-    end
-
+    Nokogiri::HTML(doc).css('.student-card')
   end
 
   def self.scrape_profile_page(profile_url)
 
   end
-
-  def test
-    Scraper.scrape_index_page('fixtures/student-site/index.html')
-  end
   binding.pry
 
 end
-
-test

d74187b1cb5754019d7cc213e2c4833241df9687
Adam Duff <j.adam.duff@gmail.com>
2018-08-20 20:08:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,15 +5,26 @@ class Student
   @@all = []
 
   def initialize(student_hash)
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @profile_url = student_hash[:profile_url]
     @@all << self
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |student|
+      new_s = Student.new(student)
+      new_s.add_student_attributes(Scraper.scrape_profile_page(new_s.profile_url))
+    end
   end
 
   def add_student_attributes(attributes_hash)
-
+    self.twitter = attributes_hash[:twitter] if attributes_hash[:twitter]
+    self.linkedin = attributes_hash[:linkedin] if attributes_hash[:linkedin]
+    self.github = attributes_hash[:github] if attributes_hash[:github]
+    self.blog = attributes_hash[:blog] if attributes_hash[:blog]
+    self.profile_quote = attributes_hash[:profile_quote] if attributes_hash[:profile_quote]
+    self.bio = attributes_hash[:bio] if attributes_hash[:bio]
   end
 
   def self.all

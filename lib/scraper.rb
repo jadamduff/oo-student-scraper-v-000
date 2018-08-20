92225f55e713ebf8bfe78aa6d154b11dc05a6aaa
Adam Duff <j.adam.duff@gmail.com>
2018-08-20 18:52:51 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,9 +5,12 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    students_hash = {}
+    students_arr = []
     doc = open(index_url)
     Nokogiri::HTML(doc).css('.student-card')
+    students.each do |student|
+      hash = {:name => student.css('a h4.student-name').text, :location => student.css('a p.student-location').text}
+    end
   end
 
   def self.scrape_profile_page(profile_url)

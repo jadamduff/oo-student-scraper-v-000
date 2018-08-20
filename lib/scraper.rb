bb82cde2687dbe53442278340d5be4db9f8eccf0
Adam Duff <j.adam.duff@gmail.com>
2018-08-20 20:44:16 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,7 +16,7 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     student_hash = {}
-    student = Nokogiri::HTML(open(profile_url))
+    student = Nokogiri::HTML(open(profile_url)).to_s
     student.css('.social-icon-container a').each do |link|
       if link.css('img.social-icon').attr('src').value.split('/').last == "twitter-icon.png"
         student_hash[:twitter] = link.attr('href')

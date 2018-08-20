6bfb61275777efc7f1e2f2e271afeb71e526e7b2
Adam Duff <j.adam.duff@gmail.com>
2018-08-20 20:45:08 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,8 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     students_arr = []
-    students = Nokogiri::HTML(open(index_url)).to_s.css('.student-card')
+    doc = open(index_url)
+    students = Nokogiri::HTML(doc).css('.student-card')
     students.each do |student|
       hash = {:name => student.css('a h4.student-name').text, :location => student.css('a p.student-location').text, :profile_url => student.css('a').attr('href').value}
       students_arr << hash
@@ -16,7 +17,8 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     student_hash = {}
-    student = Nokogiri::HTML(open(profile_url)).to_s
+    doc = open(profile_url)
+    student = Nokogiri::HTML(doc)
     student.css('.social-icon-container a').each do |link|
       if link.css('img.social-icon').attr('src').value.split('/').last == "twitter-icon.png"
         student_hash[:twitter] = link.attr('href')

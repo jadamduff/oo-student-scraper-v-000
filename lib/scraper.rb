178bd02f966744cad5c65d9fe485fa7a35db548a
Adam Duff <j.adam.duff@gmail.com>
2018-08-20 19:33:39 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,13 +24,13 @@ class Scraper
         student_hash[:twitter] = link.css('a').attr('href').value
       end
       if link.css('img.social-icon').attr('src').value.split('/').last == "linkedin-icon.png"
-        student_hash[:linkedin] = link.attr('href').value
+        student_hash[:linkedin] = link.css('a').attr('href').value
       end
       if link.css('img.social-icon').attr('src').value.split('/').last == "github-icon.png"
-        student_hash[:github] = link.attr('href').value
+        student_hash[:github] = link.css('a').attr('href').value
       end
       if link.css('img.social-icon').attr('src').value.split('/').last == "rss-icon.png"
-        student_hash[:blog] = link.attr('href').value
+        student_hash[:blog] = link.css('a').attr('href').value
       end
     end
     student_hash
